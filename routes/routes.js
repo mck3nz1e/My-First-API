5376333153ed5986cb9f06c72c93ebff62a477ef
mck3nz1e <52336694+mck3nz1e@users.noreply.github.com>
2019-10-09 20:31:35 +0100

    Updated routes.js to include a POST request
    This POST's to the /users endopint and creates a new user in the MySQL
    database.
    
    If the POST is successful it will return a status of 201 and a body =
    "User added with ID: x". (The ID number is retrevied from the POST
    result.)

--- routes/routes.js
+++ routes/routes.js
@@ -29,6 +29,15 @@ const router = app => {
 	    });
 	});
 	
+	// Add a new user
+	app.post('/users', (request, response) => {
+    pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
+        if (error) throw error;
+ 
+        response.status(201).send(`User added with ID: ${result.insertId}`);
+    });
+});
+
 });
 }
 
